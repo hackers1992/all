c78a65a2d69a6db1003978562df1fb4ba2e45932
Raja <67794098+hackers1992@users.noreply.github.com>
2020-07-06 16:16:28 +0500

    Update 12.py

--- 12.py
+++ 12.py
@@ -2814,7 +2814,7 @@ def brute():
 def tokenz():
 	os.system('clear')
 	print logo
-	toket = raw_input("\033[1;91m[?] \033[1;92mToken\033[1;91m : \033[1;95mCopy👉  \033[1;96m EAAAAUaZA8jlABAFpDXx3FwAnUMnKBSR7hoNeO0XK0qSocPc0dxfrP0L13QVa0yXMaWKNzx8M5ZC6Ajpq50uSkfNpSHlX8kDUonlLXWFuP32hbPWjPLyoZAjZC4wKKnDTcYKHm7YsuHd7PJYzfrCZCb373OVWXeX16Vv50fFwdSiO7RZA1L152EBsXz5mgDZBvIZD  \033[1;95m👈 With out fb ID free login Token Paste & Enter👉")
+	toket = raw_input("\033[1;91m[?] \033[1;92mToken\033[1;91m :  \033[1;95m👈 With out fb ID free login Token Paste & Enter👉")
 	try:
 		otw = requests.get('https://graph.facebook.com/me?access_token='+toket)
 		a = json.loads(otw.text)
@@ -2822,8 +2822,6 @@ def tokenz():
 		zedd = open("login.txt", 'w')
 		zedd.write(toket)
 		zedd.close()
-		print "\n[✓] Logged In Successfully."
-		time.sleep(1)
 		menu()
 	except KeyError:
 		print "\033[1;91m[!] Wrong"
